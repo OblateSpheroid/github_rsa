b2d9d589b0659d9db8697c48aecac79517846e9e
John <celica6@juno.com>
2018-02-12 20:34:42 -0500

    making path relative to executable

--- script.sh
+++ script.sh
@@ -1,12 +1,14 @@
 # run via : source script.sh
-if [ ! -f github_rsa ]
-  then ccat github_rsa.cpt > github_rsa
-  chmod 0600 github_rsa
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+if [ ! -f $parent_path/github_rsa ]
+  then ccat $parent_path/github_rsa.cpt > $parent_path/github_rsa
+  chmod 0600 $parent_path/github_rsa
 fi
 
 eval "$(ssh-agent -s)"
-ssh-add github_rsa
+ssh-add $parent_path/github_rsa
 
-rm github_rsa
+rm $parent_path/github_rsa
 
 # add github_rsa.pub to GitHub
